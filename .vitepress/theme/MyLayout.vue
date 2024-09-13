43c2f3e1906e5665a4e4e7cdb066a38a579638f7
Andrea Binelli <binelli.andrea@gmail.com>
2024-09-13 21:11:59 +0200

    Created & updated in page

--- .vitepress/theme/MyLayout.vue
+++ .vitepress/theme/MyLayout.vue
@@ -1,14 +1,31 @@
 <script setup>
-import DefaultTheme from 'vitepress/theme'
+import { computed } from 'vue';
+import DefaultTheme from 'vitepress/theme';
+import { useData } from 'vitepress';
 
-const { Layout } = DefaultTheme
+const { Layout } = DefaultTheme;
+const { frontmatter } = useData();
+
+const formattedCreated = computed(() => getDateText(frontmatter.value.created, true));
+const formattedUpdated = computed(() => getDateText(frontmatter.value.updated));
+
+function getDateText(ts, created) {
+  const dateString = Intl.DateTimeFormat().format(new Date(ts));
+  const result = created ? `Creato il ${dateString}` : `Aggiornato il ${dateString}`;
+
+  return result;
+}
 </script>
 
 <template>
   <Layout>
     <template #doc-before>
       <div class="vp-doc">
-        <h1>{{ $frontmatter.title }}</h1>
+        <h1>
+          {{ $frontmatter.title + ' ' }}
+          <Badge type="tip" :text="formattedCreated" />
+          <Badge type="info" :text="formattedUpdated" />
+        </h1>
       </div>
     </template>
   </Layout>
