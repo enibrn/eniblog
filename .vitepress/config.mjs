e701bdef191c48cd0e16c61a7c1f4bc39d35cb76
Andrea Binelli <binelli.andrea@gmail.com>
2024-09-15 23:53:42 +0200

    IT translations

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -7,10 +7,10 @@ import mditCustomPluginFn from './mdit-custom-plugin.mjs';
 const siteMetadata = new SiteMetadataService(getItemsFromDendronNotes);
 
 export default defineConfig({
-  transformHtml: (code, id, { pageData }) => { return code.replace('Crystal', 'CRISTALLO')},
   title: "eniblog",
   base: '/eniblog/',
   description: "enib + (b)log = eniblog",
+  lang: 'it-IT',
   themeConfig: {
     socialLinks: [
       { icon: 'github', link: 'https://github.com/enibrn/eniblog' }
@@ -18,8 +18,61 @@ export default defineConfig({
     nav: siteMetadata.nav,
     sidebar: siteMetadata.sidebar,
     search: {
-      provider: 'local'
-    }
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: 'Cerca',
+            buttonAriaLabel: 'Cerca'
+          },
+          modal: {
+            searchBox: {
+              resetButtonTitle: 'Pulisci ricerca',
+              resetButtonAriaLabel: 'Pulisci ricerca',
+              cancelButtonText: 'Annulla',
+              cancelButtonAriaLabel: 'Annulla'
+            },
+            startScreen: {
+              recentSearchesTitle: 'Cronologia delle ricerche',
+              noRecentSearchesText: 'Nessuna ricerca recente',
+              saveRecentSearchButtonTitle: 'Salva nella cronologia delle ricerche',
+              removeRecentSearchButtonTitle: 'Rimuovi dalla cronologia delle ricerche',
+              favoriteSearchesTitle: 'Preferiti',
+              removeFavoriteSearchButtonTitle: 'Rimuovi dai preferiti'
+            },
+            errorScreen: {
+              titleText: 'Impossibile ottenere risultati',
+              helpText: 'Verifica la tua connessione di rete'
+            },
+            footer: {
+              selectText: 'Seleziona',
+              navigateText: 'Naviga',
+              closeText: 'Chiudi',
+              searchByText: 'Ricerca per'
+            },
+            noResultsScreen: {
+              noResultsText: 'Impossibile trovare risultati', //seems not working
+              suggestedQueryText: 'Puoi provare una nuova ricerca',
+              reportMissingResultsText: 'Dovrebbero esserci risultati per questa query?',
+              reportMissingResultsLinkText: 'Clicca per inviare feedback'
+            }
+          }
+        }
+      }
+    },
+    //italian labels
+    docFooter: {
+      prev: 'Precedente',
+      next: 'Successivo'
+    },
+    outline: {
+      label: 'In questa pagina'
+    },
+    returnToTopLabel: 'Torna all\'inizio',
+    sidebarMenuLabel: 'Menu laterale',
+    darkModeSwitchLabel: 'Tema Scuro',
+    lightModeSwitchTitle: 'Cambia in tema chiaro',
+    darkModeSwitchTitle: 'Cambia in tema scuro'
   },
   rewrites: {
     'notes/:note': ':note'
