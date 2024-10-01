fe68c27a01a30bbecd88b562c9984335f44f2bc3
Andrea Binelli <binelli.andrea@gmail.com>
2024-10-01 23:19:17 +0200

    move configs

--- .vitepress/config/index.mjs
+++ .vitepress/config/index.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vitepress';
-import { getItemsFromDendronNotes2 } from './dendron-utilities.mjs'
-import { SiteMetadataService } from './site-metadata-service.mjs'
+import { getItemsFromDendronNotes2 } from '../dendron-utilities.mjs'
+import { SiteMetadataService } from '../site-metadata-service.mjs'
 import markdownItWikilinksFn from "markdown-it-wikilinks";
-import mditCustomPluginFn from './mdit-custom-plugin.mjs';
+import mditCustomPluginFn from '../mdit-custom-plugin.mjs';
 
 const siteMetadata = new SiteMetadataService(getItemsFromDendronNotes2);
 
